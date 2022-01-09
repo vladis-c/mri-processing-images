8caee7fb52cb06dd136bebf796d9ff3d8accdf36
Vladislav Cherkasheninov <vladcher26@yandex.ru>
2022-01-09 13:02:39 +0200

    deleting unused divs

--- src/components/T1AndFlair.jsx
+++ src/components/T1AndFlair.jsx
@@ -39,171 +39,169 @@ function T1AndFlare(props) {
 
   return (
     <Fragment>
+      <SendImageButton
+        sendInfoFucntion={() => {
+          props.switchStateHandler(setT1ImageInput)
+          postHadler(`Images of ${T1FLAIR}`)
+        }}
+        imageState={imageInput}
+        sendImageMessage={`Send Images of ${T1FLAIR}`}
+      />
       <div>
-        <SendImageButton
-          sendInfoFucntion={() => {
-            props.switchStateHandler(setT1ImageInput)
-            postHadler(`Images of ${T1FLAIR}`)
-          }}
-          imageState={imageInput}
-          sendImageMessage={`Send Images of ${T1FLAIR}`}
-        />
-        <div>
-          <div className="grid">
-            <button
-              onClick={() => {
-                props.switchStateHandler(setIsSkullStrip)
-                postHadler("Skull Strip")
-              }}
-              disabled={imageInput && !isSkullStrip ? false : true}
-            >
-              <SkullStrip />
-            </button>
-            <label id="1"></label>
-            <button
-              onClick={() => {
-                props.switchStateHandler(setIsBiasCorrection)
-                postHadler("Bias Correction")
-              }}
-              disabled={imageInput && !isBiasCorrection ? false : true}
-            >
-              <BiasCorrection />
-            </button>
-            <label id="2"></label>
-            <button
-              onClick={() => {
-                props.switchStateHandler(setIsGradientAnalysis)
-                postHadler("Gradient Analysis")
-              }}
-              disabled={imageInput && !isGradientAnalysis ? false : true}
-            >
-              <GradientAnalysis />
-            </button>
-            <label id="3"></label>
-            <button
-              onClick={() => {
-                props.switchStateHandler(setIsIntensityNorm)
-                postHadler("Intensity Normality")
-              }}
-              disabled={imageInput && !isIntensityNorm ? false : true}
-            >
-              <IntensityNorm />
-            </button>
-            <label id="4"></label>
-          </div>
-          <div className="grid">
-            <button
-              onClick={() => {
-                props.switchStateHandler(setIsVoxelBased)
-                postHadler("Voxel-based morphometry")
-              }}
-              disabled={isSkullStrip && !isVoxelBased ? false : true}
-            >
-              <VoxelBased />
-            </button>
-            <label>
-              {isSkullStrip
-                ? ""
-                : " To send data of Voxel-based Morphometry, please, send data of Skull-Strip"}
-            </label>
-            <button
-              onClick={() => {
-                props.switchStateHandler(setIsStructuralSegment)
-                postHadler("Structural Segmentation")
-              }}
-              disabled={
-                isSkullStrip && isBiasCorrection && !isStructuralSegment
-                  ? false
-                  : true
-              }
-            >
-              <StructuralSegment />
-            </button>
-            <label>
-              {isBiasCorrection && isSkullStrip
-                ? ""
-                : " To send data of Structural Segmentation, please, send data of Skull-Strip and Bias Correction"}
-            </label>
-            <button
-              onClick={() => {
-                props.switchStateHandler(setIsLesionSegmentation)
-                postHadler("Lesion Segmentation")
-              }}
-              disabled={
-                isGradientAnalysis && isIntensityNorm && !isLesionSegmentation
-                  ? false
-                  : true
-              }
-            >
-              <LesionSegment />
-            </button>
-            <label>
-              {isGradientAnalysis && isIntensityNorm
-                ? ""
-                : " To send data of Lesion Segmentation, please, send data of both Gradient Analysis and Intensity Normalisation"}
-            </label>
-          </div>
-          <div className="block">
-            <button
-              onClick={() => {
-                props.switchStateHandler(setIsTensorBased)
-                postHadler("Tensor-based morphometry")
-              }}
-              disabled={
-                isVoxelBased && isStructuralSegment && !isTensorBased
-                  ? false
-                  : true
-              }
-            >
-              <TensorBased />
-            </button>
-            <label>
-              {isVoxelBased && isStructuralSegment
-                ? ""
-                : " To send data of Tensor-based Morphometry, please, send data of both Voxel-based morphometry and Structural Segmentation"}
-            </label>
-
-            <button
-              onClick={() => {
-                props.switchStateHandler(setIsCoRegistration)
-                postHadler("Co-registration")
-              }}
-              disabled={
-                isStructuralSegment && isLesionSegmentation && !isCoRegistration
-                  ? false
-                  : true
-              }
-            >
-              <CoRegistration />
-            </button>
-            <label>
-              {isLesionSegmentation && isStructuralSegment
-                ? ""
-                : " To send data of Co-Registration, please, send data of both Structural Segmentation (from T1 Image) and Lesion Segmentation (from Flair Image)"}
-            </label>
-          </div>
-          <div className="block">
-            <label id="5"></label>
-            <button
-              onClick={() => {
-                props.switchStateHandler(setIsHyperintensity)
-                postHadler("Hyperintensity")
-              }}
-              disabled={!isHyperintensity && isCoRegistration ? false : true}
-            >
-              <Hyperintensity />
-            </button>
-            <label>
-              {isCoRegistration
-                ? ""
-                : " To send data of Hyperintensity, please, send data of Co-Registration"}
-            </label>
-          </div>
-          <GetResultButton
-            onClick={() => props.setIsResult(true)}
-            stateOfLastElement={isTensorBased && isHyperintensity}
-          />
+        <div className="grid">
+          <button
+            onClick={() => {
+              props.switchStateHandler(setIsSkullStrip)
+              postHadler("Skull Strip")
+            }}
+            disabled={imageInput && !isSkullStrip ? false : true}
+          >
+            <SkullStrip />
+          </button>
+          <label id="1"></label>
+          <button
+            onClick={() => {
+              props.switchStateHandler(setIsBiasCorrection)
+              postHadler("Bias Correction")
+            }}
+            disabled={imageInput && !isBiasCorrection ? false : true}
+          >
+            <BiasCorrection />
+          </button>
+          <label id="2"></label>
+          <button
+            onClick={() => {
+              props.switchStateHandler(setIsGradientAnalysis)
+              postHadler("Gradient Analysis")
+            }}
+            disabled={imageInput && !isGradientAnalysis ? false : true}
+          >
+            <GradientAnalysis />
+          </button>
+          <label id="3"></label>
+          <button
+            onClick={() => {
+              props.switchStateHandler(setIsIntensityNorm)
+              postHadler("Intensity Normality")
+            }}
+            disabled={imageInput && !isIntensityNorm ? false : true}
+          >
+            <IntensityNorm />
+          </button>
+          <label id="4"></label>
         </div>
+        <div className="grid">
+          <button
+            onClick={() => {
+              props.switchStateHandler(setIsVoxelBased)
+              postHadler("Voxel-based morphometry")
+            }}
+            disabled={isSkullStrip && !isVoxelBased ? false : true}
+          >
+            <VoxelBased />
+          </button>
+          <label>
+            {isSkullStrip
+              ? ""
+              : " To send data of Voxel-based Morphometry, please, send data of Skull-Strip"}
+          </label>
+          <button
+            onClick={() => {
+              props.switchStateHandler(setIsStructuralSegment)
+              postHadler("Structural Segmentation")
+            }}
+            disabled={
+              isSkullStrip && isBiasCorrection && !isStructuralSegment
+                ? false
+                : true
+            }
+          >
+            <StructuralSegment />
+          </button>
+          <label>
+            {isBiasCorrection && isSkullStrip
+              ? ""
+              : " To send data of Structural Segmentation, please, send data of Skull-Strip and Bias Correction"}
+          </label>
+          <button
+            onClick={() => {
+              props.switchStateHandler(setIsLesionSegmentation)
+              postHadler("Lesion Segmentation")
+            }}
+            disabled={
+              isGradientAnalysis && isIntensityNorm && !isLesionSegmentation
+                ? false
+                : true
+            }
+          >
+            <LesionSegment />
+          </button>
+          <label>
+            {isGradientAnalysis && isIntensityNorm
+              ? ""
+              : " To send data of Lesion Segmentation, please, send data of both Gradient Analysis and Intensity Normalisation"}
+          </label>
+        </div>
+        <div className="block">
+          <button
+            onClick={() => {
+              props.switchStateHandler(setIsTensorBased)
+              postHadler("Tensor-based morphometry")
+            }}
+            disabled={
+              isVoxelBased && isStructuralSegment && !isTensorBased
+                ? false
+                : true
+            }
+          >
+            <TensorBased />
+          </button>
+          <label>
+            {isVoxelBased && isStructuralSegment
+              ? ""
+              : " To send data of Tensor-based Morphometry, please, send data of both Voxel-based morphometry and Structural Segmentation"}
+          </label>
+
+          <button
+            onClick={() => {
+              props.switchStateHandler(setIsCoRegistration)
+              postHadler("Co-registration")
+            }}
+            disabled={
+              isStructuralSegment && isLesionSegmentation && !isCoRegistration
+                ? false
+                : true
+            }
+          >
+            <CoRegistration />
+          </button>
+          <label>
+            {isLesionSegmentation && isStructuralSegment
+              ? ""
+              : " To send data of Co-Registration, please, send data of both Structural Segmentation (from T1 Image) and Lesion Segmentation (from Flair Image)"}
+          </label>
+        </div>
+        <div className="block">
+          <label id="5"></label>
+          <button
+            onClick={() => {
+              props.switchStateHandler(setIsHyperintensity)
+              postHadler("Hyperintensity")
+            }}
+            disabled={!isHyperintensity && isCoRegistration ? false : true}
+          >
+            <Hyperintensity />
+          </button>
+          <label>
+            {isCoRegistration
+              ? ""
+              : " To send data of Hyperintensity, please, send data of Co-Registration"}
+          </label>
+        </div>
+        <GetResultButton
+          onClick={() => props.setIsResult(true)}
+          stateOfLastElement={isTensorBased && isHyperintensity}
+        />
       </div>
     </Fragment>
   )
--- src/components/T1Image.jsx
+++ src/components/T1Image.jsx
@@ -15,7 +15,7 @@ function T1Image(props) {
   const [isBiasCorrection, setIsBiasCorrection] = useState(false)
   const [isVoxelBased, setIsVoxelBased] = useState(false)
   const [isStructuralSegment, setIsStructuralSegment] = useState(false)
-  const [isTensorBased, setIsTensorBased] = useState(false)  
+  const [isTensorBased, setIsTensorBased] = useState(false)
 
   async function postHadler(prop) {
     await axios
@@ -27,97 +27,95 @@ function T1Image(props) {
 
   return (
     <Fragment>
-      <div>
       <SendImageButton
-          sendInfoFucntion={() => {
-            props.switchStateHandler(setT1ImageInput)
-            postHadler(`Images of ${T1}`)
-          }}
-          imageState={T1imageInput}
-          sendImageMessage={`Send Images of ${T1}`}
-        />
-        <div>
-          <div className="block">
-            <button
-              onClick={() => {
-                props.switchStateHandler(setIsSkullStrip)
-                postHadler("Skull Strip")
-              }}
-              disabled={T1imageInput && !isSkullStrip ? false : true}
-            >
-              <SkullStrip />
-            </button>
-            <label></label>
-            <button
-             onClick={() => {
+        sendInfoFucntion={() => {
+          props.switchStateHandler(setT1ImageInput)
+          postHadler(`Images of ${T1}`)
+        }}
+        imageState={T1imageInput}
+        sendImageMessage={`Send Images of ${T1}`}
+      />
+      <div>
+        <div className="block">
+          <button
+            onClick={() => {
+              props.switchStateHandler(setIsSkullStrip)
+              postHadler("Skull Strip")
+            }}
+            disabled={T1imageInput && !isSkullStrip ? false : true}
+          >
+            <SkullStrip />
+          </button>
+          <label></label>
+          <button
+            onClick={() => {
               props.switchStateHandler(setIsBiasCorrection)
               postHadler("Bias Correction")
             }}
-              disabled={T1imageInput && !isBiasCorrection ? false : true}
-            >
-              <BiasCorrection />
-            </button>
-            <label></label>
-          </div>
-          <div className="block">
-            <button
-              onClick={() => {
-                props.switchStateHandler(setIsVoxelBased)
-                postHadler("Voxel-based morphometry")
-              }}
-              disabled={isSkullStrip && !isVoxelBased ? false : true}
-            >
-              <VoxelBased />
-            </button>
-            <label>
-              {isSkullStrip
-                ? ""
-                : " To send data of Voxel-based Morphometry, please, send data of Skull-Strip"}
-            </label>
-            <button
-              onClick={() => {
-                props.switchStateHandler(setIsStructuralSegment)
-                postHadler("Structural Segmentation")
-              }}
-              disabled={
-                isSkullStrip && isBiasCorrection && !isStructuralSegment
-                  ? false
-                  : true
-              }
-            >
-              <StructuralSegment />
-            </button>
-            <label>
-              {isBiasCorrection && isSkullStrip
-                ? ""
-                : " To send data of Structural Segmentation, please, send data of Skull-Strip and Bias Correction"}
-            </label>
-          </div>
-          <div className="block">
-            <button
-              onClick={() => {
-                props.switchStateHandler(setIsTensorBased)
-                postHadler("Tensor-based morphometry")
-              }}
-              disabled={
-                isVoxelBased && isStructuralSegment && !isTensorBased
-                  ? false
-                  : true
-              }
-            >
-              <TensorBased />
-            </button>
-            <label>
-              {isVoxelBased && isStructuralSegment
-                ? ""
-                : " To send data of Tensor-based Morphometry, please, send data of both Voxel-based morphometry and Structural Segmentation"}
-            </label>
-          </div>
-          <GetResultButton
-            onClick={() => props.setIsResult(true)}
-            stateOfLastElement={isTensorBased}
-          />
+            disabled={T1imageInput && !isBiasCorrection ? false : true}
+          >
+            <BiasCorrection />
+          </button>
+          <label></label>
         </div>
+        <div className="block">
+          <button
+            onClick={() => {
+              props.switchStateHandler(setIsVoxelBased)
+              postHadler("Voxel-based morphometry")
+            }}
+            disabled={isSkullStrip && !isVoxelBased ? false : true}
+          >
+            <VoxelBased />
+          </button>
+          <label>
+            {isSkullStrip
+              ? ""
+              : " To send data of Voxel-based Morphometry, please, send data of Skull-Strip"}
+          </label>
+          <button
+            onClick={() => {
+              props.switchStateHandler(setIsStructuralSegment)
+              postHadler("Structural Segmentation")
+            }}
+            disabled={
+              isSkullStrip && isBiasCorrection && !isStructuralSegment
+                ? false
+                : true
+            }
+          >
+            <StructuralSegment />
+          </button>
+          <label>
+            {isBiasCorrection && isSkullStrip
+              ? ""
+              : " To send data of Structural Segmentation, please, send data of Skull-Strip and Bias Correction"}
+          </label>
+        </div>
+        <div className="block">
+          <button
+            onClick={() => {
+              props.switchStateHandler(setIsTensorBased)
+              postHadler("Tensor-based morphometry")
+            }}
+            disabled={
+              isVoxelBased && isStructuralSegment && !isTensorBased
+                ? false
+                : true
+            }
+          >
+            <TensorBased />
+          </button>
+          <label>
+            {isVoxelBased && isStructuralSegment
+              ? ""
+              : " To send data of Tensor-based Morphometry, please, send data of both Voxel-based morphometry and Structural Segmentation"}
+          </label>
+        </div>
+        <GetResultButton
+          onClick={() => props.setIsResult(true)}
+          stateOfLastElement={isTensorBased}
+        />
       </div>
     </Fragment>
   )
